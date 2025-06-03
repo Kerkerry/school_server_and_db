b4c99b8486d6fe2e1c7120e3499dd5fffc5bd372
Kerkerry <kerrywangamati24@gmail.com>
2025-06-03 12:33:00 +0300

    Corrected the error of returning null results

--- controllers/courses_controller.js
+++ controllers/courses_controller.js
@@ -3,12 +3,17 @@ import connection from "../models/connection.js";
 const courses=(req,res)=>{
     try {
         connection.query(
-            `SELECT * FROM courses`,
+            `SELECT 
+            cs.course_code,cs.course_name,cs.description,u.username instructor,cs.schedule,cs.prerequisites,
+            cs.credits,cs.level,cs.term,cs.delivery_method,cs.syllabus_url,cs.required_textbooks,cs.start_date,cs.end_date
+            FROM courses cs
+            JOIN users u
+            ON cs.instructor_id=u.user_id`,
             (err,result)=>{
                 if(err){
-                    res.send({statusCode:200,body:result})
+                    res.send({statusCode:500,error:err})  
                 }else{
-                    res.send({statusCode:500,error:err})
+                    res.send({statusCode:200,body:result})
                 }
             }
         )
@@ -21,13 +26,19 @@ const course=(req,res)=>{
     const id=req.params.id;
      try {
         connection.query(
-            `SELECT * FROM courses WHERE course_id = ?`,
+            `SELECT 
+            cs.course_code,cs.course_name,cs.description,u.username instructor,cs.schedule,cs.prerequisites,
+            cs.credits,cs.level,cs.term,cs.delivery_method,cs.syllabus_url,cs.required_textbooks,cs.start_date,cs.end_date
+            FROM courses cs
+            JOIN users u
+            ON cs.instructor_id=u.user_id
+            WHERE course_id = ?`,
             [id],
             (err,result)=>{
                 if(err){
-                    res.send({statusCode:200,body:result})
-                }else{
                     res.send({statusCode:500,error:err})
+                }else{
+                    res.send({statusCode:200,body:result})
                 }
             }
         )
