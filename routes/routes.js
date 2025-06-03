1c10e9a9f81712100041d4395b7e7ecd932f72d6
Kerkerry <kerrywangamati24@gmail.com>
2025-06-03 12:41:24 +0300

    Added routes for courses'

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,22 @@
 import scontroller from "../controllers/students_controller.js";
 import acontroller from "../controllers/app_controllers.js";
 import instructors from "../controllers/instructors_controller.js";
+import courses_controller from "../controllers/courses_controller.js";
 import express from 'express'
 
 const Router=express.Router();
 Router.get("/", acontroller.index);
+// Student
 Router.get('/students',scontroller.students)
 Router.get("/students/:id",scontroller.student)
+// Course Materails
 Router.get("/course_materials",scontroller.course_materials)
 Router.get("/course_materials/:id",scontroller.course_material)
+// Instructors
 Router.get('/instructors',instructors.instructors)
 Router.get('/instructors/:id',instructors.instructor_courses)
+// Courses
+Router.get("/courses",courses_controller.courses)
+Router.get("/courses/:id",courses_controller.course)
 
 export default Router;
